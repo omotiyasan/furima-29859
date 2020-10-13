1f0464b98a0bbf58c6f80df0a9ea103dcc171610
omotiyasan <sb.uv07ys@gmail.com>
2020-10-13 16:24:55 +0900

    商品出品機能の実装

--- db/migrate/20200925053024_create_products.rb
+++ db/migrate/20200925053024_create_products.rb
@@ -1,10 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string :name
-      t.string :text
-      t.text :image
-      t.timestamps
+      t.string :name,               null: false, default: ""
+      t.string :description,        null: false, default: ""
+      t.integer :category_id,       null: false
+      t.integer :condition_id,      null: false
+      t.integer :shippingcharge_id, null: false
+      t.integer :salesarea_id,      null: false
+      t.integer :daystoship_id,     null: false
+      t.integer :money,             null: false
+      t.references :user,           foreign_key: true
     end
   end
 end
